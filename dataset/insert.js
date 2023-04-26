bf780bd208b42b0e91136343547c625c350d8d1c
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2023-04-26 04:01:35 +0200

    update dataset

--- dataset/insert.js
+++ dataset/insert.js
@@ -1,51 +1,52 @@
-import fs from "fs/promises"
-import mysql from "mysql2/promise"
-import env from "dotenv"
-import express from "express"
+import fs from "fs/promises";
+import mysql from "mysql2/promise";
+import env from "dotenv";
+import express from "express";
 
-env.config()
+env.config();
 
-const app = express()
-const connect = await mysql.createConnection(process.env.DATABASE_URL)
+const app = express();
+const connect = await mysql.createConnection(process.env.DATABASE_URL);
 
 let dataStudents, dataGrades, dataTeachers;
 
 try {
-	let res = await fs.readFile("dataset/students.json", "utf8")
-	dataStudents = JSON.parse(res)
-	res = await fs.readFile("dataset/grades.json", "utf8")
-	dataGrades = JSON.parse(res)
-	res = await fs.readFile("dataset/teachers.json", "utf8")
-	dataTeachers = JSON.parse(res)
+	let res = await fs.readFile("dataset/students.json", "utf8");
+	dataStudents = JSON.parse(res);
+	res = await fs.readFile("dataset/grades.json", "utf8");
+	dataGrades = JSON.parse(res);
+	res = await fs.readFile("dataset/teachers.json", "utf8");
+	dataTeachers = JSON.parse(res);
 } catch (err) {
-	console.log(err)
+	console.log(err);
 }
 
 app.get('/', async (req, res) => {
-    // await connect.query(`INSERT into students values (1,'a','b',1,'ss','s')`)
-    res.send()
-})
+	// await connect.query(`INSERT into students values (1,'a','b',1,'ss','s')`)
+	res.send();
+});
 
 app.get('/seedStudents', async (req, res) => {
 	for (let i = 0; i < dataStudents.length; i++) {
-		const { student_id, first_name, last_name, group_id, email, phone_number } = dataStudents[i]
-		await connect.query(`INSERT INTO students VALUES (?,?,?,?,?,?)`, [student_id, first_name, last_name, group_id, email, phone_number])
+		const { student_id, first_name, last_name, group_id, email, phone_number } = dataStudents[i];
+		await connect.query(`INSERT INTO students VALUES (?,?,?,?,?,?)`, [student_id, first_name, last_name, group_id, email, phone_number]);
 	}
-	res.send("seeding successfully completed")
-})
+	res.send("seeding successfully completed");
+});
 
 app.get("/seedGrades", (req, res) => {
+	console.log(dataGrades.length);
 	dataGrades.forEach(record => {
-		connect.query(`INSERT INTO grades VALUES (?, ?, ?, ?, ?)`, [record.student_id, record.group_id, record.subject_id, record.grade, record.status])
+		connect.query(`INSERT INTO grades VALUES (?, ?, ?, ?, ?)`, [record.student_id, record.group_id, record.subject_id, record.grade, record.status]);
 	});
-	res.send()
-})
+	res.send();
+});
 
 app.get("/seedTeacher", (req, res) => {
 	dataTeachers.forEach(record => {
-		connect.query(`INSERT INTO teacher VALUES (?, ?, ?, ?)`, [record.teacher_id, record.first_name, record.last_name, record.email])
+		connect.query(`INSERT INTO teacher VALUES (?, ?, ?, ?)`, [record.teacher_id, record.first_name, record.last_name, record.email]);
 	});
-	res.send()
-})
+	res.send();
+});
 
-app.listen(3000, console.log("server listening on port 3030"))
+app.listen(3000, console.log("server listening on port 3030"));
