96d4eb25c5feb2dab48ef8fcd223e01c39002859
Khaled <Khaled195341@feng.bu.edu.eg>
2023-04-27 19:31:03 +0200

    using axios in ui=> it's just a comment ya esraa

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,51 +1,45 @@
-import React, { Component } from "react"
+import React, { Component, useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
 import "./bootstrap/css/bootstrap.css"
-import Header from "./components/Header";
-import Selection from "./components/Selection";
-import { Route, Routes, BrowserRouter, Link } from 'react-router-dom';
-import Group1 from './components/Group1';
-import Group2 from './components/Group2';
-import Main from "./components/Main";
+import Header from "./components/Header"
+import Selection from "./components/Selection"
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom"
+import Group1 from "./components/Group1"
+import Group2 from "./components/Group2"
+import Main from "./components/Main"
+import axios from "axios"
 
 function App() {
+	// console.log(process.env, process.env.REACT_APP_BASR_URL)
+	// const [backendData, setBackendData] = useState([{}])
+	// useEffect(() => {
+	// 	axios.get(`${process.env.REACT_APP_BASR_URL}/students`).then((data) => {
+	// 		console.log(data.data)
+	// 		setBackendData(data.data)
+	// 		// debugger
+	// 	})
+	// }, [])
 
-  return (
+	return (
+		// <div>{JSON.stringify(backendData)}</div>
+		<BrowserRouter>
+			<div>
+				<main>
+					<switch>
+						<Routes>
+							<Route path="" element={<Main />} />
+							<Route path="/select" element={<Selection />} />
 
-    <BrowserRouter>
-      <div>
+							<Route path="/Group1" element={<Group1 />} />
+							<Route path="/Group2" element={<Group2 />} />
 
-   
-
-        
-<main>
-
-<switch>
-<Routes>
-<Route path="" element={<Main />} />
-<Route path="/select" element={<Selection />} />
-
-<Route path="/Group1" element={<Group1 />} />
-<Route path="/Group2" element={<Group2 />} />
-
-{/* <Route path="/loginpass" element={<Loginpass />} /> */}
-
-</Routes>
-</switch>
-
-</main>
-       
-      </div>
-    </BrowserRouter>
-
-
-
-
-
-
-
-  );
-
+							{/* <Route path="/loginpass" element={<Loginpass />} /> */}
+						</Routes>
+					</switch>
+				</main>
+			</div>
+		</BrowserRouter>
+	)
 }
 
 export default App
