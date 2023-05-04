a093ed9e135dee20decda567f9b5f9e84d1f514c
Samar-Mahmoud <samar195444@feng.bu.edu.eg>
2023-05-04 20:29:26 +0300

    fix

--- ui/src/components/ShowStudentInfoup.js
+++ ui/src/components/ShowStudentInfoup.js
@@ -1,15 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import "../bootstrap/css/bootstrap.css";
-import Header from './Header';
-import Options from './Options';
-import { getAllStudents, } from './Options';
-import FunctionsOp from './FunctionsOp';
-import { Await, json } from 'react-router-dom';
-import GetAllStudents from './GetAllStudents';
-import GetInfoGroupStudents from './GetInfoGroupStudents';
-import GetOneGroupStudents from './GetOneGroupStudents';
-import { groupid } from './Groups';
 import { group } from './Groups';
 
 export default function ShowStudentInfoup() {
@@ -166,7 +157,7 @@ export default function ShowStudentInfoup() {
                         Add
                     </a>
                 </form>
-                {acadmeic()}
+                {/* {acadmeic()} */}
             </div>
 
         </div>
