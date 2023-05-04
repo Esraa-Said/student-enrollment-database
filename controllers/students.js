2b579b3d6b3bf6c0880fe580095bf60b3699d3cf
Khaled <Khaled195341@feng.bu.edu.eg>
2023-05-04 21:03:18 +0300

    update grades issue

--- controllers/students.js
+++ controllers/students.js
@@ -1,38 +1,59 @@
-/*
-this file will be used to add functionalty 
-like function to add , create ,delete , ... student 
-*/
-
 import { db } from "../db/connect.js";
 import { StatusCodes } from "http-status-codes";
 
 export const getAllStudents = async (req, res) => {
-    const [data] = await db.query(`select *, group_name from students, groups where groups.group_id = students.group_id`);
+    const [data] = await db.query(
+        `select *, group_name from students, groups where groups.group_id = students.group_id`
+    );
     res.status(200).json({ data, length: data.length });
 };
 
 export const getStudentById = async (req, res) => {
     const id = req.params.id;
     try {
-        const [data] = await db.query(`select * from students where student_id = ?`, id);
+        const [data] = await db.query(
+            `select * from students where student_id = ?`,
+            id
+        );
         res.status(200).json(data);
     } catch (err) {
-        res.status(StatusCodes.BAD_REQUEST).json({ msg: `Student with ID = ${id} is not found` });
+        res.status(StatusCodes.BAD_REQUEST).json({
+            msg: `Student with ID = ${id} is not found`,
+        });
     }
 };
 
 export const createStudent = async (req, res) => {
-    const { student_id, first_name, last_name, group_id, email, phone_number } = req.body;
+    const { student_id, first_name, last_name, group_id, email, phone_number } =
+        req.body;
     let val;
     try {
-        val = await db.query(`INSERT INTO students VALUES (?,?,?,?,?,?)`, [student_id, first_name, last_name, group_id, email, phone_number]);
-        const [subject] = await db.query(`select subject_id ,subject_name from subjects where group_id = ?`, Number(group_id));
+        val = await db.query(`INSERT INTO students VALUES (?,?,?,?,?,?)`, [
+            student_id,
+            first_name,
+            last_name,
+            group_id,
+            email,
+            phone_number,
+        ]);
+        const [subject] = await db.query(
+            `select subject_id ,subject_name from subjects where group_id = ?`,
+            Number(group_id)
+        );
         for (let i = 0; i < subject.length; i++) {
             let subject_id = subject[i].subject_id;
-            await db.query(`INSERT INTO grades VALUES (?, ?, ?, ?, ?)`, [Number(student_id), Number(group_id), Number(subject_id), Number(0), "failed"]);
+            await db.query(`INSERT INTO grades VALUES (?, ?, ?, ?, ?)`, [
+                Number(student_id),
+                Number(group_id),
+                Number(subject_id),
+                Number(0),
+                "failed",
+            ]);
         }
     } catch (err) {
-        return res.json({ msg: "Error in creating student. Please, check data validation and try again" });
+        return res.json({
+            msg: "Error in creating student. Please, check data validation and try again",
+        });
     }
     res.status(StatusCodes.CREATED).json(req.body);
 };
@@ -49,26 +70,62 @@ export const updateStudent = async (req, res, next) => {
     if (email) updateObj.email = email;
     if (phone_number) updateObj.phone_number = phone_number;
 
+    const [subject] = await db.query(
+        `select subject_id ,subject_name from subjects where group_id = ?`,
+        Number(group_id)
+    );
+    for (let i = 0; i < subject.length; i++) {
+        let subject_id = subject[i].subject_id;
+        try {
+            await db.query(`INSERT INTO grades VALUES (?, ?, ?, ?, ?)`, [
+                Number(student_id),
+                Number(group_id),
+                Number(subject_id),
+                Number(0),
+                "failed",
+            ]);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     for (let [k, v] of Object.entries(updateObj)) {
-        await db.query(`UPDATE students SET ${k} = ? WHERE student_id = ? `, [v, student_id]);
+        await db.query(`UPDATE students SET ${k} = ? WHERE student_id = ? `, [
+            v,
+            student_id,
+        ]);
     }
-    const [result] = await db.query(`SELECT * FROM students WHERE student_id = ?`, student_id);
+    const [result] = await db.query(
+        `SELECT * FROM students WHERE student_id = ?`,
+        student_id
+    );
     if (!result.length) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ msg: `Student with ID = ${student_id} is not found` });
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ msg: `Student with ID = ${student_id} is not found` });
     }
+    console.log(result);
     res.status(StatusCodes.OK).send(result);
 };
 
 export const deleteStudent = async (req, res) => {
     const student_id = req.params.id;
-    const [result] = await db.query(`SELECT * FROM students WHERE student_id = ?`, student_id);
+    const [result] = await db.query(
+        `SELECT * FROM students WHERE student_id = ?`,
+        student_id
+    );
     if (!result.length) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ msg: `Student with ID = ${student_id} is not found` });
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ msg: `Student with ID = ${student_id} is not found` });
     }
     await db.query(`DELETE FROM students WHERE student_id = ?`, student_id);
     const group_id = result.group_id;
     try {
-        await db.query(`DELETE FROM grades WHERE group_id = ?  AND student_id = ?`, [group_id, student_id]);
+        await db.query(
+            `DELETE FROM grades WHERE group_id = ?  AND student_id = ?`,
+            [group_id, student_id]
+        );
     } catch (err) {
         console.log(err);
     }
@@ -80,40 +137,55 @@ export const updateGrade = async (req, res) => {
     const { grade } = req.body;
     let status = grade >= 50 ? "succeeded" : "failed";
     try {
-        let result = await db.query(`UPDATE grades SET grade = (?) , status = (?)  WHERE student_id = ? AND group_id = ? AND subject_id = ?`, [
-            grade,
-            status,
-            Number(student_id),
-            Number(group_id),
-            Number(subject_id),
-        ]);
+        let result = await db.query(
+            `UPDATE grades SET grade = (?) , status = (?)  WHERE student_id = ? AND group_id = ? AND subject_id = ?`,
+            [
+                grade,
+                status,
+                Number(student_id),
+                Number(group_id),
+                Number(subject_id),
+            ]
+        );
     } catch (err) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ msg: `Grade updation failed` });
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ msg: `Grade updation failed` });
     }
     res.json({ msg: `Grade updation is successfully done` });
 };
 
 export const setGrades = async (req, res) => {
     const { gid: group_id, sid: student_id } = req.params;
-    const [subject] = await db.query(`select subject_id ,subject_name from subjects where group_id = ?`, Number(group_id));
+    const [subject] = await db.query(
+        `select subject_id ,subject_name from subjects where group_id = ?`,
+        Number(group_id)
+    );
     let subjectNameToId = new Map();
-    for (let i = 0; i < subject.length; i++) subjectNameToId.set(subject[i].subject_name, subject[i].subject_id);
+    for (let i = 0; i < subject.length; i++)
+        subjectNameToId.set(subject[i].subject_name, subject[i].subject_id);
 
     try {
         for (let [subject_name, new_grade] of Object.entries(req.body)) {
             const subject_id = subjectNameToId.get(subject_name);
             const new_status = new_grade >= 50 ? "succeeded" : "failed";
-            await db.query(`UPDATE grades SET grade = (?) , status = (?)  WHERE student_id = ? AND group_id = ? AND subject_id = ?`, [
-                new_grade,
-                new_status,
-                Number(student_id),
-                Number(group_id),
-                Number(subject_id)
-            ]);
+            await db.query(
+                `UPDATE grades SET grade = (?) , status = (?)  WHERE student_id = ? AND group_id = ? AND subject_id = ?`,
+                [
+                    new_grade,
+                    new_status,
+                    Number(student_id),
+                    Number(group_id),
+                    Number(subject_id),
+                ]
+            );
         }
+    } catch (err) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ msg: `Grade updation failed` });
     }
-    catch (err) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ msg: `Grade updation failed` });
-    }
-    res.status(StatusCodes.OK).json({ msg: `Grade updation is successfully done` });
+    res.status(StatusCodes.OK).json({
+        msg: `Grade updation is successfully done`,
+    });
 };
